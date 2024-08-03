c07857d0f01fedbcda10896054134f7362574d97
Alexander Aghili <alexander.w.aghili@gmail.com>
2024-08-02 21:14:16 -0700

    test script fix

--- perf_test.sh
+++ perf_test.sh
@@ -5,14 +5,14 @@ run_visualizer() {
     local c=$1
     local p=$2
     echo "Running: ./julia_set_visualizer -p $p -c $c" 
-    ./julia_set_visualizer -p "$p" -c "$c" > /dev/null
+    ./JuliaSetVisualizer/julia_set_visualizer -p "$p" -c "$c" > /dev/null
 }
 
 export -f run_visualizer
 
 # Use GNU parallel to run the commands in parallel
 #parallel --verbose run_visualizer ::: {3..5096} ::: {1..5}
-for i in {3..5096}; do
+for i in {1..110}; do
     for j in {1..5}; do
         run_visualizer $i $j
     done
